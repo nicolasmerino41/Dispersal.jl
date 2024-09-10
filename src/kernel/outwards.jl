7cea49bd6374a9344f71afb67a93d6ed8abd1825
nicomerino <nicolasmerino41@gmail.com>
2024-09-10 17:14:38 +0200

    Update outwards.jl

--- src/kernel/outwards.jl
+++ src/kernel/outwards.jl
@@ -116,9 +116,9 @@ end
         (target_mod, inbounds) = DynamicGrids.inbounds(data, target)
 
         if inbounds && (isnothing(mask_data) || mask_data[target_mod...])
-            @inbounds propagules = k .* body_mass_vector * N   # Apply the adjusted kernel
+            @inbounds propagules = k .* body_mass_vector .* N.a   # Apply the adjusted kernel
             @inbounds add!(data[W], propagules, target_mod...)  # Add to neighboring cell
-            sum += propagules
+            sum += MyStructs256(SVector{256, Float64}(propagules))
         end
     end
     
